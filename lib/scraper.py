864894f7ac5deab34ed5922154c314935991efb1
Linet <muthiigathoni@gmail.com>
2023-09-23 13:54:21 +0300

    Complete

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,12 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+try:
+    doc = BeautifulSoup(html.text, 'html.parser')
+    courses = doc.select('.heading-25-extrabold.color-black')
+
+    for course in courses:
+        print(course.contents[0].strip())
+except Exception as e:
+    print("An error occurred:", e)
